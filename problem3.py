1137dd08db77d147beb806be6098886e715379fa
begumyivli <begumm0532@icloud.com>
2023-07-17 21:45:06 +0200

    first push

--- problem3.py
+++ problem3.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.optimize import minimize, Bounds, LinearConstraint
 import matplotlib.pyplot as plt
+#deneme
 
 def objective(y:np.ndarray, Z:int, p:float, M:int, N:int, avg_aois:dict={}) -> float:
     """
@@ -50,7 +51,7 @@ def objective(y:np.ndarray, Z:int, p:float, M:int, N:int, avg_aois:dict={}) -> f
 
 M = 2  # Number of updates
 N = 1  # Time interval
-p_list = [0.5, 0.52, 0.54, 0.56, 0.58, 0.6, 0.63, 0.65, 0.68, 0.7, 0.73, 0.75, 0.77, 0.79, 0.81, 0.83, 0.85, 0.87, 0.89, 0.92, 0.95, 0.97, 1]
+p_list = [0.5, 0.55, 0.58, 0.6, 0.63, 0.65, 0.68, 0.7, 0.73, 0.75, 0.77, 0.79, 0.81, 0.83, 0.85, 0.87, 0.89, 0.92, 0.95, 0.97, 1]
 Z_list = [1,2,5,10]
 AoIs = {}
 
